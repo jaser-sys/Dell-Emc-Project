c19aae75596c6de14c191b80e9b4dddb259b294e
abeer282 <abeerala282@gmail.com>
2020-01-29 14:04:19 +0200

    added h2 DB properties

--- myServer/src/main/java/com/server/MyServerApplication.java
+++ myServer/src/main/java/com/server/MyServerApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 @EnableSwagger2
 public class MyServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyServerApplication.class, args);
 	}
-
 }
