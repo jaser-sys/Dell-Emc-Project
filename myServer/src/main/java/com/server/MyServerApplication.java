8dd21c011cb4366d995377f01bf498e5a5518c3a
abeer282 <abeerala282@gmail.com>
2020-01-29 09:52:32 +0200

    added swagger2

--- myServer/src/main/java/com/server/MyServerApplication.java
+++ myServer/src/main/java/com/server/MyServerApplication.java
@@ -3,7 +3,10 @@ package com.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MyServerApplication {
 
 	public static void main(String[] args) {
