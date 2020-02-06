beef4578747c8dbe8e1f105e5e5ac81fa4b59fd0
nidaa <nida.alk94@gmail.com>
2020-02-06 15:17:05 +0200

    add method addNewEntry

--- myServer/myServer/src/main/java/com/server/FileQuery.java
+++ myServer/myServer/src/main/java/com/server/FileQuery.java
@@ -1,5 +1,21 @@
 package com.server;
-
-public class FileQuery {
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+public class FileQuery extends SqliteDB{
+	String sqlCommand = "INSERT INTO files (vmID, Name, creationDate, Location) "
+	 		+ "VALUES("+ ((File)o).getVmID() +",\'" + ((File)o).getName() + "\',\'" + ((File)o).getSize() + "\',\'" + ((File)o).getCreationDate() +"\',\'" + ((File)o).getLocation() + "\');" ;
+	
+	 try (Connection conn = super.connect();
+			 PreparedStatement stmt  = conn.prepareStatement(sqlCommand) ){				 
+		 stmt.executeUpdate();
+        }
+	 catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+}
 }
