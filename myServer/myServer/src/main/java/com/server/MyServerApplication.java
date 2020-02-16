363598f20f0de64571e0ff960022d0db9a9adb5b
abeer282 <abeerala282@gmail.com>
2020-02-16 13:10:23 +0200

    added base package of controllers

--- myServer/myServer/src/main/java/com/server/MyServerApplication.java
+++ myServer/myServer/src/main/java/com/server/MyServerApplication.java
@@ -2,10 +2,15 @@ package com.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import controllers.VirtualMachineController;
+
+
 @SpringBootApplication
 @EnableSwagger2
+@ComponentScan(basePackages = { "controllers"} )
 public class MyServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyServerApplication.class, args);
