76d4548dc30db77db3662c5e7680776b34374ef9
abeer282 <abeerala282@gmail.com>
2020-02-17 15:40:12 +0200

    changed columns names

--- myServer/myServer/src/main/java/dao/VirtualMachineQuery.java
+++ myServer/myServer/src/main/java/dao/VirtualMachineQuery.java
@@ -15,8 +15,8 @@ import model.VirtualMachine;
 public class VirtualMachineQuery extends SqliteDB {
 	
 	public void addNewEntry(Object o) {
-		String sqlCommand = "INSERT INTO virtualMachines (vmID, vmHostName, vmUsername, vmPassword) "
-		 		+ "VALUES("+ ((VirtualMachine)o).getID() +",\'" + ((VirtualMachine)o).getIP() + "\',\'" + ((VirtualMachine)o).getUserName() + "\',\'" + ((VirtualMachine)o).getPassword() + "\');" ;
+		String sqlCommand = "INSERT INTO virtualMachines (ID, IP, userName, password_) "
+		 		+ "VALUES(\'"+ ((VirtualMachine)o).getID() +"\',\'" + ((VirtualMachine)o).getIP() + "\',\'" + ((VirtualMachine)o).getUserName() + "\',\'" + ((VirtualMachine)o).getPassword() + "\');" ;
 		
 		 try (Connection conn = super.connect();
 				 PreparedStatement stmt  = conn.prepareStatement(sqlCommand) ){				 
@@ -36,10 +36,10 @@ public class VirtualMachineQuery extends SqliteDB {
 	             ResultSet result    = stmt.executeQuery(sqlCommand)){
 	            while (result.next()) {
 	            	VirtualMachine v1=new VirtualMachine();
-	            	v1.setID(UUID.fromString(result.getString("vmID")));
-	            	v1.setIP(result.getString("vmHostName"));
-	            	v1.setUserName(result.getString("vmUsername"));
-	            	v1.setPassword(result.getString("vmPassword"));
+	            	v1.setID(UUID.fromString(result.getString("ID")));
+	            	v1.setIP(result.getString("IP"));
+	            	v1.setUserName(result.getString("userName"));
+	            	v1.setPassword(result.getString("password_"));
 	            	myVirtualMachines.add((Object)v1);
 	            }
 	        } catch (SQLException e) {
@@ -49,7 +49,7 @@ public class VirtualMachineQuery extends SqliteDB {
 	}
 	
 	public void deleteEntryByID(int entryId) {
-		String sql = "DELETE FROM virtualMachines WHERE vmID = " + entryId +";";  
+		String sql = "DELETE FROM virtualMachines WHERE ID = " + entryId +";";  
 		
         try (Connection conn = this.connect();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -59,8 +59,8 @@ public class VirtualMachineQuery extends SqliteDB {
         }	
 	}
 	
-	public void updatePasswordByID(int vmID,String newPassword) {
-		String sqlCommand = "UPDATE virtualMachines SET vmPassword=\""+newPassword+"\" WHERE vmID = "+vmID+";";
+	public void updatePasswordByID(UUID vmID,String newPassword) {
+		String sqlCommand = "UPDATE virtualMachines SET password_=\""+newPassword+"\" WHERE ID = \'" + vmID.toString() + "\';";
 		
         try (Connection conn = this.connect();
                 PreparedStatement pstmt = conn.prepareStatement(sqlCommand)) {
