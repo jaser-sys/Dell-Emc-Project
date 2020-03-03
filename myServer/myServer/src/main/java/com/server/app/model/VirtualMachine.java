6bacd75fb9cedad8971f876fba9d5ab0ef29578d
abeer282 <abeerala282@gmail.com>
2020-03-03 23:36:05 +0200

    Add user id

--- myServer/myServer/src/main/java/com/server/app/model/VirtualMachine.java
+++ myServer/myServer/src/main/java/com/server/app/model/VirtualMachine.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 public class VirtualMachine {
 	private UUID ID;
+	private UUID userId;
 	private String IP;
 	private String userName;
 	private String password;
@@ -13,54 +14,53 @@ public class VirtualMachine {
 		
 	}
 	
-	public VirtualMachine(String ip,String username, String password)
-	{
+	public VirtualMachine(UUID userId,String ip,String username, String password){
 		this.ID = UUID.randomUUID();
+		this.userId = userId;
 		this.IP = ip;
 		this.userName = username;
 		this.password = password;
 	}
-	
 	
 	public UUID getID() {
 		return this.ID;
 	}
 	
-	
 	public void setID(UUID id) {
 		this.ID = id;
 	}
-
 
 	public String getIP() {
 		return this.IP;
 	}
 
-
 	public void setIP(String ip) {
 		this.IP = ip;
 	}
-
 
 	public String getUserName() {
 		return this.userName;
 	}
 
-
 	public void setUserName(String username) {
 		this.userName = username;
 	}
-
 
 	public String getPassword() {
 		return this.password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	
+	public UUID getUserId() {
+		return userId;
+	}
+
+	public void setUserId(UUID userId) {
+		this.userId = userId;
+	}
+
 }
 
