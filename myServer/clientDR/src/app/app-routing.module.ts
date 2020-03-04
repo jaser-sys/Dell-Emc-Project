f8c7683dc3773191a123ed46790d9b26ad55bd0e
nidaa <nida.alk94@gmail.com>
2020-03-04 14:32:06 +0200

    add route to login page

--- myServer/clientDR/src/app/app-routing.module.ts
+++ myServer/clientDR/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: UserLoginComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
