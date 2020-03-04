ee9e6b68db9e24fb484d936be2ed7f5b5475a53d
user <mariamalatrash1@gmail.com>
2020-03-04 17:05:16 +0200

    added register html and TS

--- myServer/clientDR/src/app/create-user/create-user.component.ts
+++ myServer/clientDR/src/app/create-user/create-user.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { UserService } from '../user.service';
+import { HttpClient, HttpResponse, HttpEventType } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
@@ -8,9 +12,24 @@ import { User } from '../user';
 export class CreateUserComponent implements OnInit {
     user: User = new User();
     submitted = false;
-  constructor() { }
+  constructor(private userService: UserService,
+    private router: Router) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+      save() {
+          this.userService.userLogin(this.user)
+              .subscribe(event => {
+                  if (event instanceof HttpResponse) {
+                      console.log('user added!')
+                  }
+              });
+      }
+  
+  
+      onSubmit() {
+          this.submitted = true;
+          this.save();
+      }
 
 }
