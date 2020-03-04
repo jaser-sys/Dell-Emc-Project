b0d22e707d8d5a7369c08bfaec39a4a3fd605167
nidaa <nida.alk94@gmail.com>
2020-03-04 15:48:39 +0200

    declare user at create user

--- myServer/clientDR/src/app/create-user/create-user.component.ts
+++ myServer/clientDR/src/app/create-user/create-user.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../user';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent implements OnInit {
-
+    user: User = new User();
+    submitted = false;
   constructor() { }
 
   ngOnInit(): void {
