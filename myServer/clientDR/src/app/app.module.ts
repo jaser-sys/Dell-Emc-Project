e595b7bd6ae1db33f65e27226066e126be124709
nidaa <nida.alk94@gmail.com>
2020-03-04 12:20:58 +0200

    add new component user-login

--- myServer/clientDR/src/app/app.module.ts
+++ myServer/clientDR/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
