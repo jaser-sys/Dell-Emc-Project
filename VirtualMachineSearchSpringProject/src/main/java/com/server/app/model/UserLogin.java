cd6d5f5b605970b8be9f7d848ccf527fd4b326f6
nidaa <nida.alk94@gmail.com>
2020-03-08 13:18:49 +0200

    add new model for user login

---
+++ VirtualMachineSearchSpringProject/src/main/java/com/server/app/model/UserLogin.java
@@ -0,0 +1,35 @@
+package com.server.app.model;
+
+public class UserLogin {
+
+	private String username;
+	private String password;
+
+	
+	public UserLogin() {
+		
+	}
+	
+	public UserLogin(String username_, String password_) {
+		this.username=username_;
+		this.password=password_;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
