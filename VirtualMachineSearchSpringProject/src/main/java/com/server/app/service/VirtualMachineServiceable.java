89014b889693836903bd444340391822d7070172
abeer282 <abeerala282@gmail.com>
2020-03-11 14:06:26 +0200

    Update return value and argument type

--- VirtualMachineSearchSpringProject/src/main/java/com/server/app/service/VirtualMachineServiceable.java
+++ VirtualMachineSearchSpringProject/src/main/java/com/server/app/service/VirtualMachineServiceable.java
@@ -6,10 +6,10 @@ import java.util.UUID;
 import com.server.app.model.VirtualMachine;
 
 public interface  VirtualMachineServiceable {
-    public List<VirtualMachine> getVirtualMachineByIp(String ip);
+    public VirtualMachine getVirtualMachineByIp(String ip);
 	public List<VirtualMachine> getVirtualMachineByUsername(String username);
 	public List<VirtualMachine> getVitualMachines(String username);
 	public void addVirtualMachine(UUID userId, String ip, String username, String password);
 	public void deleteVirtualMachineByIp(String ip);
-	public String scanVirtualMachineByIp(String ip);
+	public String scanVirtualMachineByIp(VirtualMachine vm);
 }
