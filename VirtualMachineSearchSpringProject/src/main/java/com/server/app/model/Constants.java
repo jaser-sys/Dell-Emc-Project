afe447ac68c1c09d48c7d8dbcb12c2a41937bfcd
nidaa23 <54308358+nidaa23@users.noreply.github.com>
2020-03-17 19:25:45 +0200

    update key

--- VirtualMachineSearchSpringProject/src/main/java/com/server/app/model/Constants.java
+++ VirtualMachineSearchSpringProject/src/main/java/com/server/app/model/Constants.java
@@ -2,7 +2,7 @@ package com.server.app.model;
 
 public class Constants {
     public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
-    public static final String SIGNING_KEY = "9Sh2zIqwFlMYx0DgtVrdN53PmQbCAKep";
+    public static final String SIGNING_KEY = "9Sh2zIqwFlMfgdfdfxsjjsOSMDssnnsuew554Yx0DgtVrdN53PmQbCAKep";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
