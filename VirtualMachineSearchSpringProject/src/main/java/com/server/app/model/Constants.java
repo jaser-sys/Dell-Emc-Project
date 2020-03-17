c0355a97af52257d6c39b38940528a2db136218c
nidaa <nida.alk94@gmail.com>
2020-03-17 14:03:26 +0200

    Add class models Const, AuthTpke, APIRes for jwt auth. update pom

---
+++ VirtualMachineSearchSpringProject/src/main/java/com/server/app/model/Constants.java
@@ -0,0 +1,8 @@
+package com.server.app.model;
+
+public class Constants {
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
+    public static final String SIGNING_KEY = "9Sh2zIqwFlMYx0DgtVrdN53PmQbCAKep";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+}
