51e97b7b9513ace59c5f34490e9f14aed9152cb0
nidaa <nida.alk94@gmail.com>
2020-03-17 14:45:46 +0200

    add Authentication controller

---
+++ VirtualMachineSearchSpringProject/src/main/java/com/server/app/controller/AuthenticationController.java
@@ -0,0 +1,49 @@
+package com.server.app.controller;
+import com.server.app.config.authentication.JwtTokenUtil;
+import com.server.app.model.ApiResponse;
+import com.server.app.model.AuthToken;
+import com.server.app.model.User;
+import com.server.app.model.UserLogin;
+import  com.server.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/token")
+ class AuthenticationController {
+
+	@Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+    public ApiResponse<AuthToken> login(@RequestBody UserLogin loginUser) throws AuthenticationException {
+    	
+    	 System.out.println(loginUser.getPassword());
+    	 System.out.println(loginUser.getUsername());
+   
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUser.getUsername(), loginUser.getPassword()));
+   
+        final User user = userService.loadUserByUsername(loginUser.getUsername());
+     
+        final String token = jwtTokenUtil.generateToken(user);
+
+        return new ApiResponse<>(200, "success",new AuthToken(token, user.getUsername_()));
+	
+    }
+	
+}
