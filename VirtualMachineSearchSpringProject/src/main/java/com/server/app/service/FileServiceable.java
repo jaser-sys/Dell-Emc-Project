84718c417ac331a07eae2e6004918dbb83a1b2fe
nidaa <nida.alk94@gmail.com>
2020-04-03 01:06:03 +0300

    update getFilesByName method

--- VirtualMachineSearchSpringProject/src/main/java/com/server/app/service/FileServiceable.java
+++ VirtualMachineSearchSpringProject/src/main/java/com/server/app/service/FileServiceable.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.server.app.model.File;
 
 public interface FileServiceable {
-	public List<File> getFilesByFileName(List<String> IP,String fileName) throws Exception;
+	public List<File> getFilesByFileName(String IP,String fileName) throws Exception;
 	public List<File> getFilesBySize(String IP,int size) throws Exception;
 	public List<File> retFilesByDateMax(String IP,String m_Date) throws Exception;
 	public List<File> getFilesByDateBtw(String IP,String f_Date, String t_Date) throws Exception;
