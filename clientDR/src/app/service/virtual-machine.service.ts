a2e979fbca99c23687c33977a368b153fd8ae7a6
nidaa <nida.alk94@gmail.com>
2020-04-03 03:11:16 +0300

    add new methods to vm service

--- clientDR/src/app/service/virtual-machine.service.ts
+++ clientDR/src/app/service/virtual-machine.service.ts
@@ -2,18 +2,32 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 import { virtualMachine } from '../model/virtualMachine';
+import { ApiResponse } from '../model/api.response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VirtualMachineService {
 
-  private baseUrl = 'http://localhost:8080/vm';
+    private baseUrl = 'http://localhost:8080/vm';
+    private url: string;
   constructor(private http: HttpClient) { }
 
 
   addVM(VirtualMachine: Object): Observable<Object> {
       return this.http.post(`${this.baseUrl}/${'post'}`, virtualMachine);
   }
+    getListVms(username: string): Observable<ApiResponse> {
+        this.url = this.baseUrl + '/listVM';
+        return this.http.get<ApiResponse>(`${this.url}/${username}`);
+    }
 
+    updateVMs(username: string, vm: virtualMachine): Observable<ApiResponse> {
+        this.url = this.baseUrl + '/updateVM';
+        return this.http.put<ApiResponse>(`${this.url}/${username}`, vm);
+    }
+    deleteVMByIP(ip: string): Observable<ApiResponse> {
+        this.url = this.baseUrl + '/deleteVM';
+        return this.http.delete<ApiResponse>(`${this.url}/${ip}`);
+    }
 }
