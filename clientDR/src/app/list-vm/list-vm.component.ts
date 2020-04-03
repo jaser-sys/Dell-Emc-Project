c159d935eacda74bae19d996a5758175ee6af10f
nidaa <nida.alk94@gmail.com>
2020-04-03 03:38:52 +0300

    add methods update,delete,list

--- clientDR/src/app/list-vm/list-vm.component.ts
+++ clientDR/src/app/list-vm/list-vm.component.ts
@@ -23,5 +23,35 @@ export class ListVmComponent implements OnInit {
             return;
         }
   }
+    public listFiles(VM: virtualMachine): void {
+        window.localStorage.setItem('IP', VM.ip);
+        this.router.navigate(['file/listfile']);
 
+    };
+    public addVM() {
+        this.router.navigate(['addVM']);
+    };
+
+    public scanAll(): void {
+        window.localStorage.removeItem("scanVM");
+        window.localStorage.setItem("scanVM", "all");
+        this.router.navigate(['file/filter']);
+    };
+
+    public scanVM(VM: virtualMachine): void {
+        window.localStorage.removeItem("scanVM");
+        window.localStorage.setItem("scanVM", VM.ip.toString());
+        this.router.navigate(['file/filter']);
+    };
+    public updateVM(VM: virtualMachine): void {
+        window.localStorage.removeItem("updateVM");
+        window.localStorage.setItem("updateVM", VM.ip.toString());
+        this.router.navigate(['vm/update-vm']);
+    };
+    public deleteVM(VM: virtualMachine): void {
+        this.vmService.deleteVMByIP(VM.ip)
+            .subscribe(data => {
+                this.vms = this.vms.filter(v => v !== VM);
+            })
+    };
 }
