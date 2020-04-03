4f392c522b3db68e5d84439849db9c423c698361
nidaa <nida.alk94@gmail.com>
2020-04-04 02:34:47 +0300

    add class,imports providers to app module to change date format

--- clientDR/src/app/app.module.ts
+++ clientDR/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MomentDateModule, MomentDateAdapter } from '@angular/material-moment-adapter';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { CreateVmComponent } from './create-vm/create-vm.component';
@@ -16,6 +17,21 @@ import { SearchFileComponent } from './search-file/search-file.component';
 import { ListFileComponent } from './list-file/list-file.component';
 import { ListVmComponent } from './list-vm/list-vm.component';
 import { ListFileMultiComponent } from './list-file-multi/list-file-multi.component';
+import * as moment from 'moment';
+
+
+
+export const DateFormats = {
+    parse: {
+        dateInput: ['YYYY-MM-DD']
+    },
+    display: {
+        dateInput: 'YYYY-MM-DD',
+        monthYearLabel: 'MMM YYYY',
+        dateA11yLabel: 'LL',
+        monthYearA11yLabel: 'MMMM YYYY',
+    },
+};
 
 @NgModule({
   declarations: [
@@ -39,7 +55,12 @@ import { ListFileMultiComponent } from './list-file-multi/list-file-multi.compon
       MatNativeDateModule,
       MatDatepickerModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [ 
+        { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+        { provide: MAT_DATE_FORMATS, useValue: DateFormats }
+    ],
+  
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
