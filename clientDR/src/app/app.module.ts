3c012baa002aceb8831b8111879ddfda15fdd2fb
nidaa <nida.alk94@gmail.com>
2020-04-04 00:52:17 +0300

    install angular material library

--- clientDR/src/app/app.module.ts
+++ clientDR/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatNativeDateModule, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { CreateVmComponent } from './create-vm/create-vm.component';
@@ -25,7 +30,12 @@ import { ListVmComponent } from './list-vm/list-vm.component';
     BrowserModule,
       AppRoutingModule,
       FormsModule,
-      HttpClientModule
+      HttpClientModule,
+      MatInputModule,
+      MatRadioModule,
+      MatSelectModule,
+      MatNativeDateModule,
+      MatDatepickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
