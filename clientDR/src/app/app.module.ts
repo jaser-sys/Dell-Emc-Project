b3a7c2f0164f0bf9884d94e454ec089b102c6ec2
nidaa <nida.alk94@gmail.com>
2020-04-04 02:44:13 +0300

    Add imports to app module

--- clientDR/src/app/app.module.ts
+++ clientDR/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS,  HttpClientModule } from "@angular/common/http";
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatNativeDateModule, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
@@ -17,6 +20,10 @@ import { SearchFileComponent } from './search-file/search-file.component';
 import { ListFileComponent } from './list-file/list-file.component';
 import { ListVmComponent } from './list-vm/list-vm.component';
 import { ListFileMultiComponent } from './list-file-multi/list-file-multi.component';
+import { TokenInterceptor } from "./core/interceptor";
+import { UserService } from "./service/user.service";
+import { VirtualMachineService } from './service/virtual-machine.service';
+import { FileService } from './service/file.service';
 import * as moment from 'moment';
 
 
@@ -44,22 +51,29 @@ export const DateFormats = {
     ListVmComponent,
     ListFileMultiComponent
   ],
-  imports: [
-    BrowserModule,
-      AppRoutingModule,
-      FormsModule,
-      HttpClientModule,
-      MatInputModule,
-      MatRadioModule,
-      MatSelectModule,
-      MatNativeDateModule,
-      MatDatepickerModule,
+    imports: [
+
+     BrowserModule,
+     AppRoutingModule,
+     FormsModule,
+     ReactiveFormsModule,
+     BrowserAnimationsModule,
+     HttpClientModule,
+     MatInputModule,
+     MatRadioModule,
+     MatSelectModule,
+     MatNativeDateModule,
+     MatDatepickerModule,
   ],
-    providers: [ 
+    providers: [UserService, FileService, VirtualMachineService, {
+        provide: HTTP_INTERCEPTORS,
+        useClass: TokenInterceptor,
+        multi: true
+    },
         { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
         { provide: MAT_DATE_FORMATS, useValue: DateFormats }
     ],
-  
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
     bootstrap: [AppComponent]
 })
 
