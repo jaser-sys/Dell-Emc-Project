b3957bf463a91abd79bd715fa84e61f3491a764e
nidaa <nida.alk94@gmail.com>
2020-04-03 22:05:01 +0300

    update method getOnInit, check auth call method to get list

--- clientDR/src/app/list-file/list-file.component.ts
+++ clientDR/src/app/list-file/list-file.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient, HttpResponse, HttpEventType } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { file } from '../model/file';
+import { FileService } from '../service/file.service';
 @Component({
   selector: 'app-list-file',
   templateUrl: './list-file.component.html',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListFileComponent implements OnInit {
 
-  constructor() { }
+    files: File[];
+    ip: string;
+    type: string;
 
-  ngOnInit(): void {
-  }
+    constructor(private router: Router, private fileService: FileService) { }
 
+    ngOnInit(): void {
+        if (!window.localStorage.getItem('token') || !window.localStorage.getItem('username') ||
+            !window.localStorage.getItem('scanVM')) {
+
+            this.router.navigate(['login']);
+            return;
+        } else {
+            const isIp = window.localStorage.getItem('scanVM').toString();
+            if (isIp.length > 6) {
+                console.log("here at list ips");
+                this.fileService.getListToCompList()
+                    .subscribe((data) => {
+
+                        this.files = data.result as File[];
+                        console.table(this.files);
+                    });
+
+
+            } else {
+                console.log("not found");
+            }
+        }
+    }
 }
