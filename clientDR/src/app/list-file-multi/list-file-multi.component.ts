d137d0e3dcc23858c2b45a9fdcc6939bf517eba0
nidaa <nida.alk94@gmail.com>
2020-04-04 03:20:47 +0300

    add imports, update getOnInit method in list-file-multi

--- clientDR/src/app/list-file-multi/list-file-multi.component.ts
+++ clientDR/src/app/list-file-multi/list-file-multi.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { file } from '../model/file';
+import { FileService } from '../service/file.service';
+import { HttpClient, HttpResponse, HttpEventType } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-list-file-multi',
   templateUrl: './list-file-multi.component.html',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListFileMultiComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router, private fileService: FileService) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+        if (!window.localStorage.getItem('token') || !window.localStorage.getItem('username') ||
+            !window.localStorage.getItem('scanVM')) {
+
+            this.router.navigate(['user/login']);
+            return;
+        } 
   }
 
 }
